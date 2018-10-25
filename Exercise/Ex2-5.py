bc99d65948206c684bfddbb46654fd0c1e9c6894
music820717 <music820717@gmail.com>
2018-10-26 01:37:27 +0800

    add exercise 2-5 to 3-3

---
+++ Exercise/Ex2-5.py
@@ -0,0 +1,5 @@
+celsius = float(input("Enter a Celsius temperature: "))
+fahrenheit = celsius * 9/5 + 32
+print("Fahrenheit = ",round(fahrenheit,2))
+
+#Variable的字首小寫較好
---
+++ Exercise/Ex3-1.py
@@ -0,0 +1,9 @@
+hr = eval(input("Enter Hours: "))
+rt = eval(input("Enter Rates: "))
+
+if hr < 40 and hr > 0 :
+  wage = hr*rt
+else :
+  wage = 40*rt + (hr-40)*1.5*rt
+
+print("Pay:",round(wage,1))

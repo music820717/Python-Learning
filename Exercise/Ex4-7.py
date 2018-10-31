907e7b3ad8bb7b51986f074100809877d6102ead
music820717 <music820717@gmail.com>
2018-11-01 01:11:34 +0800

    add exercise 4-7

--- Exercise/Ex4-7.py
+++ Exercise/Ex4-7.py
@@ -1,26 +1,22 @@
-score = input('Enter score: ')
-
-def computegrade(score) :
+def computegrade(score):
   try:
     score = float(score)
+
+    if score > 1:
+      print('Please enter valid score')
+    elif score > 0.9 and score <= 1:
+      print('A')
+    elif score > 0.8 and score <= 0.9:
+      print('B')
+    elif score > 0.7 and score <= 0.8:
+      print('C')
+    elif score > 0.6 and score <= 0.7:
+      print('D')
+    else:
+      print('F')
+  
   except:
-    print('Please enter valid scores')
+    print('Please enter valid score')
 
-  if score > 1 :
-    print('Please enter valid scores')
-  elif score > 0.9 and score <= 1 :
-    print('A')
-  elif score > 0.8 and score <=0.9 :
-    print('B')
-  elif score > 0.7 and score <=0.8 :
-    print('C')
-  elif score > 0.6 and score <= 0.7 :
-    print('D')
-  else :
-    print('F')
-
-
+score = input('Enter score: ')
 computegrade(score)
-
-
-#Unsolved!!!
